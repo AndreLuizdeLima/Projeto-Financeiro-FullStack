7383bfc8024b82d69699039d235c08a25eb6b3ff
AndreLuizdeLima <andreluizjp2015@gmail.com>
2026-09-19 16:34:22 -0300

    preparando entidade user

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,13 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id!: number;
 
+  @Column({ length: 100 })
   nome!: string;
 
+  @Column({ unique: true })
   email!: string;
 
+  @Column({ length: 255 })
   passwordHash!: string;
 
+  @CreateDateColumn()
   createDate!: Date;
 
+  @UpdateDateColumn()
   updateDate?: Date;
 }

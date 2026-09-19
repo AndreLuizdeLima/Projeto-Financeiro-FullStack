53ad09d17ffc45370a545c1447c3f6f4f4bd9259
AndreLuizdeLima <andreluizjp2015@gmail.com>
2026-09-19 18:17:12 -0300

    criando rotas de users

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,26 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '@/auth/hashing/hashing.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const passwordHash = await this.hashingService.hash(
+        createUserDto.password,
+      );
+      const newUser = this.userRepository.create({
+        nome: createUserDto.nome,
+        email: createUserDto.email,
+        passwordHash,
+      });
+      await this.userRepository.save(newUser);
+      const { passwordHash: _passwordHash, ...userReturn } = newUser;
+
+      return userReturn;
+    } catch {
+      throw new InternalServerErrorException('Divergência ao criar usuário');
+    }
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepository.find({
+      take: 100,
+      order: { id: 'DESC' },
+    });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userRepository.findOne({ where: { id } });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const { password, ...userData } = updateUserDto;
+
+    const user = await this.userRepository.preload({
+      id,
+      ...userData,
+    });
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrada.');
+    }
+
+    if (password) {
+      user.passwordHash = await this.hashingService.hash(password);
+    }
+
+    return this.userRepository.save(user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrada.');
+    }
+    return this.userRepository.remove(user);
   }
 }
